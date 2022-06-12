8fb3a303627b5b053f60d5ce5c4c346e30054e5d
Nyanthie Mayang Gatluak <nyanthie.mayang@student.moringaschool.com>
2022-06-12 16:35:33 +0300

    commit tree aefea97ada295b89dff7f8dfee487d56f1480a39

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+const employee = {
+    name: `Gako`,
+    streetAddress:`210721`
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+   return Object.assign({},employee,{[key]:value});
+
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key]=value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key){
+    const secondEmp ={...employee};
+    delete secondEmp[key];
+    return secondEmp;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
